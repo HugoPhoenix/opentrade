c1c61d46c6c9f3dc712779517e998550b25efaf1
HugoPhoenix <77645698+HugoPhoenix@users.noreply.github.com>
2021-04-07 09:36:00 +0930

    Update private_constants.js

--- server/modules/private_constants.js
+++ server/modules/private_constants.js
@@ -1,7 +1,7 @@
 'use strict';
 
-exports.recaptcha_priv_key = 'YOUR_GOOGLE_RECAPTCHA_PRIVATE_KEY';
-exports.password_private_suffix = 'LONG_RANDOM_STRING1';
+exports.recaptcha_priv_key = 'XXX';
+exports.password_private_suffix = 'XXX';
 exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
 exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
 
@@ -10,9 +10,11 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 if (exports.password_private_suffix == 'LONG_RANDOM_STRING1') throw new Error('You MUST change default value exports.password_private_suffix !')
 
 exports.walletspassphrase = {
-    'MC' : 'LONG_RANDOM_STRING2',
-    'BTC' : 'LONG_RANDOM_STRING3',
-    'DOGE' : 'LONG_RANDOM_STRING4'
+    'PA' : 'XXX',
+    'LTC' : 'XXX',
+    'PIVX' : 'XXX',
+    'BTC' : 'XXX',
+    'DOGE' : 'XXX'
 };
 
 exports.IsUnlimitedAddress = function(addr)
